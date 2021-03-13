4acfe276f2fb615f0123a9454c272d92295750c0
FuegoDev <77174551+FuegoDev@users.noreply.github.com>
2021-03-13 00:33:59 -0800

    Initial Commit

---
+++ Physics1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physics1 : MonoBehaviour
+{
+    public Rigidbody rb;
+   
+    void FixedUpdate()
+    {
+        rb.AddForce(10, 10, 10);
+    }
+}
